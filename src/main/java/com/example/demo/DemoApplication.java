4dc520fae8daecf229ca1f9428c44a4d3ab6377e
Maciej Arkit <maciej.arkit@gmail.com>
2019-07-31 01:47:18 +0200

    Use JdbcTemplate to list shops from database.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	private final JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	public DemoApplication(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
 	}
 
 	@GetMapping("/")
@@ -18,4 +27,10 @@ public class DemoApplication {
 		return "Spring Boot and Google App Engine - Hello World!";
 	}
 
+	@GetMapping("/shops")
+	public List<String> getShops() {
+		return this.jdbcTemplate.queryForList("SELECT * FROM shops").stream()
+				.map((m) -> m.values().toString())
+				.collect(Collectors.toList());
+	}
 }

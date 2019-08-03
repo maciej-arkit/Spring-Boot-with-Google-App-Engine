cd7eac18bff09bb1237838a2f95548651a2c93a1
Maciej Arkit <maciej.arkit@gmail.com>
2019-08-03 17:13:46 +0200

    Add Shop class.

---
+++ src/main/java/com/example/demo/Shop.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Shop {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shop shop = (Shop) o;
+        return Objects.equals(name, shop.name) &&
+                Objects.equals(address, shop.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address);
+    }
+}

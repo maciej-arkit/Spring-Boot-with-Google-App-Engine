5c1a02d1c3892eb647dbb6c9a536af168ea70e32
Maciej Arkit <maciej.arkit@gmail.com>
2019-08-06 02:58:07 +0200

    Normalize Shop model

--- src/main/java/com/example/demo/Shop.java
+++ src/main/java/com/example/demo/Shop.java
@@ -1,17 +1,13 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name="shops")
 public class Shop {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String address;

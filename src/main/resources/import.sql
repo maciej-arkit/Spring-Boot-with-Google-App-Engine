3335f550776a2abef23918c306321078c6251aa9
Maciej Arkit <maciej.arkit@gmail.com>
2019-08-06 02:53:57 +0200

    Add initial data SQL script

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO shop (name, address) values ('Shop-1', 'Test Street, New York');
+INSERT INTO shop (name, address) values ('Shop-2', 'Test Avenue, New York');
+-- Password is "password"
+INSERT INTO user (email, password, username) values ('test@example.com', '$2a$10$zY0UtMQiV.8UBcYFxc4Dse/rxAnZeyP6Hioei1pfR9T0uVTpzr3Pm', 'user');

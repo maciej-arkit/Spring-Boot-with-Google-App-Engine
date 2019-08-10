8e3bcf212b3b3b2f977b9857f9337192fd287f85
Maciej Arkit <maciej.arkit@gmail.com>
2019-08-11 00:06:04 +0200

    Add Lombok / Remove getters and setters.

--- src/main/java/com/example/demo/security/models/Role.java
+++ src/main/java/com/example/demo/security/models/Role.java
@@ -5,27 +5,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Role implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String role;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
--- src/main/java/com/example/demo/security/models/User.java
+++ src/main/java/com/example/demo/security/models/User.java
@@ -1,11 +1,16 @@
 package com.example.demo.security.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.Set;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,44 +21,4 @@ public class User implements Serializable {
 
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
